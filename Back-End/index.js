4d9635b4e8b6250575e4d5b18a047969c3a064a3
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-06-25 17:25:55 +0530

    Donee

--- Back-End/index.js
+++ Back-End/index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../Front-End/dist")));
-app.get("*", (req, res) => {
+app.get('/*any', (req, res) => {
   res.sendFile(path.join(__dirname, "../Front-End/dist/index.html"));
 });
 
