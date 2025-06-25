ccf1196ca1e9537e4bcc1bf21e498f170da7ff86
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-06-25 17:52:17 +0530

    Final

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js"
-import leetRoutes from "./routes/leetRoutes.js"
+//import leetRoutes from "./routes/leetRoutes.js"
 import axios from "axios";
 import cors from "cors"
 import path from 'path';
@@ -17,10 +17,10 @@ app.use(cors({
   origin: "https://codetrackerr.onrender.com",
   credentials: true,
 }));
+app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../Front-End/dist")));
 app.get('/*any', (req, res) => {
@@ -33,7 +33,7 @@ app.get('/*any', (req, res) => {
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // parses URL-encoded data
 
-app.use(express.json());
+a//pp.use(express.json());
 app.use("/l",leetRoutes);
 app.use("/api",authRoutes);
 app.get("/api/protect-route", verifyToken, (req, res) => {
@@ -41,7 +41,22 @@ app.get("/api/protect-route", verifyToken, (req, res) => {
 });
 app.use("/review",reviewRoutes);
 app.use("/users",userRoutes);
-app.get("/lc/:lcUsername",async (req,res)=>{
+app.get("/github/:username", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const response = await axios.get(`https://github-contributions-api.jogruber.de/v4/${username}`);
+    
+    if (!response.data) {
+      return res.status(404).json({ message: "No data found" });
+    }
+
+    return res.json(response.data);
+  } catch (error) {
+    console.error("GitHub API error:", error.message);
+    return res.status(500).json({ error: "Failed to fetch GitHub data" });
+  }
+});app.get("/lc/:lcUsername",async (req,res)=>{
     try {
         const lcUsername = req.params.lcUsername;
         const response = await axios.get(`https://leetcode-api-pied.vercel.app/user/${lcUsername}`);
