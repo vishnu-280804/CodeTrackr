645a0f536f6886a83c235b495571d7a7f697241b
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-06-25 17:13:02 +0530

    Final Commit

--- Back-End/index.js
+++ Back-End/index.js
@@ -18,9 +18,10 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../Front-End/dist')));
-app.get('/*any', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Front-End/dist/', 'index.html'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'Front-End/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front-End/dist/index.html'));
 });
 
 
