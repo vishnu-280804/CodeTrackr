26acd6501218732a2f65cf991d9dc65228ba43fb
vishnu <143005718+vishnu-280804@users.noreply.github.com>
2025-06-25 15:41:28 +0530

    Built npm in frontend

--- Back-End/index.js
+++ Back-End/index.js
@@ -17,9 +17,9 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../Front-End/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../Front-End/dist', 'index.html'));
 });
 
 
